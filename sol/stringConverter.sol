0eeb3cdf0678150bba7956f4004e9c02771af2e7
sam bacha <sam@freighttrust.com>
2020-08-22 04:45:16 -0700

    Create stringConverter.sol

---
+++ sol/stringConverter.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.11;
+
+/**
+ * @title StringConverterLib
+ * @dev String convertions to multiple types
+ */
+ 
+library StringConverterLib {
+
+    /**
+     * @dev Returns bytes32 representation of a given string
+     */
+    function strToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+    
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+}
